7fec8b708bcc19e2fae9a9ad7395455f984630e0
Timothy Holmes <tim.a.holmes@gmail.com>
2024-03-07 08:24:28 +1100

    reduce tests to minimum

--- tests/test_merge_gis.py
+++ tests/test_merge_gis.py
@@ -1,5 +1,5 @@
-from merge_gis import __version__
+# from gww_gis_tools.merge_gis import __version__
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+# def test_version():
+#     assert __version__ == '0.1.0'
